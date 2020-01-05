e3a3993df1e8544b9f6150162fe00cfc2fd330c4
Joseph Nelson <joseph.nelson2012@gmail.com>
2020-01-05 01:35:40 -0500

    roboflow export format update
    added export/_annotations.txt for train.txt and export/_classes.txt for model_data/voc_classes.txt'

--- train.py
+++ train.py
@@ -14,9 +14,9 @@ from yolo3.utils import get_random_data
 
 
 def _main():
-    annotation_path = 'train.txt'
+    annotation_path = './export/_annotations.txt'
     log_dir = 'logs/000/'
-    classes_path = 'model_data/voc_classes.txt'
+    classes_path = './export/_classes.txt'
     anchors_path = 'model_data/yolo_anchors.txt'
     class_names = get_classes(classes_path)
     num_classes = len(class_names)
